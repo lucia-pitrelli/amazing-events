f00a0f8ad31f4455cbdf30373b8b105c299bf46e
Lucia Pitrelli <pitrelliblucia@gmail.com>
2023-09-12 10:27:51 -0300

    create stats.js

--- stats.js
+++ stats.js
@@ -1,1 +1,24 @@
-let events = data.events;
+let events = [];
+
+fetch("https://mindhub-xj03.onrender.com/api/amazing")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.events);
+    showNames(data.events);
+    showcategories(data.events);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+function showNames(array) {
+  for (let object of array) {
+    console.log(object.name);
+  }
+}
+
+function showcategories(array) {
+  for (let object of array) {
+    console.log(object.category);
+  }
+}
